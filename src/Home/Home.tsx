edcb7b2b42347a90325aad5c270745da0d1d96e6
LeoGonzaga <sacredlotus05@gmail.com>
2021-05-06 19:23:05 -0300

    estrutura inicial e tsx

---
+++ src/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container } from "./styled";
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <h2>ahsdisahduih</h2>
+    </Container>
+  );
+};
+
+export default Home;
